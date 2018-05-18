6fe31e0e957696b736dd20b044415cdca1e5e84a
Shuntaro Tsukazaki <shuntaro@uw.edu>
2018-05-18 08:46:59 -0700

    do participation 8

--- exercise-1/ui.R
+++ exercise-1/ui.R
@@ -1,36 +1,53 @@
 # ui.R
 library(shiny)
 library(plotly)
-shinyUI(navbarPage(
+library(shinythemes)
+shinyUI(navbarPage(theme = shinytheme("sandstone"),
   "Electoral College",
   # Create a tab panel for your map
   tabPanel(
-    "Map",
-    titlePanel("Electoral College Votes"),
-    # Create sidebar layout
-    sidebarLayout(
-
-      # Side panel for controls
-      sidebarPanel(
-
-        # Input to select variable to map
-        selectInput(
-          "mapvar",
-          label = "Variable to Map",
-          choices = list(
-            "Population" = "population",
-            "Electoral Votes" = "votes",
-            "Votes / Population" = "ratio"
+     "Map",
+      titlePanel("Electoral College Votes"),
+  # Create sidebar layout
+  sidebarLayout(
+                       
+  # Side panel for controls
+  sidebarPanel(
+     tags$h2("This is an interactive map"),
+                         
+  # Input to select variable to map
+      selectInput(
+        "mapvar",
+         label = "Variable to Map",
+         choices = list(
+         "Population" = "population",
+         "Electoral Votes" = "votes",
+         "Votes / Population" = "ratio"
           )
-        )
       ),
-
-      # Main panel: display plotly map
-      mainPanel(
-        plotlyOutput("map")
+      tags$br(),
+      HTML("<strong>Wow, I'm blown away!</strong>"),
+      tags$div(class = "sidebar",
+          tags$blockquote("I'm a floating blockquote!"))
+  ),
+                       
+   # Main panel: display plotly map
+    mainPanel(
+        plotlyOutput("map"),
+        tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+        tags$div(class = "summary", checked = NA,
+          tags$p("This map shows the electoral college votes by states. The darker
+                 the color, the higher the number of votes. California has 
+                   the highest votes.")
+        ),
+        tags$a(href = "https://www.archives.gov/federal-register/electoral-college/about.html",
+                                  "for more information")
       )
     )
-  )
+  ),
 
-  # Create a tabPanel to show your scatter plot
+# Create a tabPanel to show your scatter plot
+  tabPanel("other",
+     tags$h1("This page is complietely empty")
+  )
 ))
